28174e264fbdc50f26b792361534fb4fa3041160
maxwellchaos <maxwellchaos@gmail.com>
2022-04-17 16:03:56 +0300

    все хорошо

---
+++ testVk/testVk/Response.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testVk
+{
+    class getProfileInfo
+    {
+        public Response response;
+        public class Response
+        {
+            public string id;
+            public string first_name;
+            public string last_name;
+            public string status_1223;
+        }
+    }
+    class getAvatar
+    {
+        public Response[] response;
+        public class Response
+        {
+            public string photo_50;
+        }
+    }
+
+
+    public class FriendsGet
+    {
+        public Response response { get; set; }
+
+        public class Item
+        {
+            public int id { get; set; }
+            public string first_name { get; set; }
+            public string last_name { get; set; }
+            public bool can_access_closed { get; set; }
+            public bool is_closed { get; set; }
+            public string photo_50 { get; set; }
+            public string track_code { get; set; }
+            public List<int> lists { get; set; }
+        }
+
+        public class Response
+        {
+            public int count { get; set; }
+            public List<Item> items { get; set; }
+        }
+    }
+
+}
