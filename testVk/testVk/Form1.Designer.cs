87629b05a50a95a2985fccf52c214a6cf056017e
maxwellchaos <maxwellchaos@gmail.com>
2022-04-17 15:51:24 +0300

    первая загрузка кода

---
+++ testVk/testVk/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace testVk
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UserInfoButton = new System.Windows.Forms.Button();
+            this.StartWebBrowser = new System.Windows.Forms.WebBrowser();
+            this.AccessTokenTextBox = new System.Windows.Forms.TextBox();
+            this.LastNameLabel = new System.Windows.Forms.Label();
+            this.AvatarPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.AvatarPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UserInfoButton
+            // 
+            this.UserInfoButton.Location = new System.Drawing.Point(25, 31);
+            this.UserInfoButton.Name = "UserInfoButton";
+            this.UserInfoButton.Size = new System.Drawing.Size(75, 23);
+            this.UserInfoButton.TabIndex = 0;
+            this.UserInfoButton.Text = "button1";
+            this.UserInfoButton.UseVisualStyleBackColor = true;
+            this.UserInfoButton.Click += new System.EventHandler(this.button1_Click);
+            this.UserInfoButton.Paint += new System.Windows.Forms.PaintEventHandler(this.button1_Paint);
+            // 
+            // StartWebBrowser
+            // 
+            this.StartWebBrowser.Location = new System.Drawing.Point(468, 31);
+            this.StartWebBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.StartWebBrowser.Name = "StartWebBrowser";
+            this.StartWebBrowser.Size = new System.Drawing.Size(345, 294);
+            this.StartWebBrowser.TabIndex = 2;
+            this.StartWebBrowser.Url = new System.Uri("https://oauth.vk.com/authorize?client_id=8104769&display=page&redirect_uri=https:" +
+        "//oauth.vk.com/blank.html&scope=friends&response_type=token&v=5.131&state=123456" +
+        "", System.UriKind.Absolute);
+            this.StartWebBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // AccessTokenTextBox
+            // 
+            this.AccessTokenTextBox.Location = new System.Drawing.Point(12, 357);
+            this.AccessTokenTextBox.Name = "AccessTokenTextBox";
+            this.AccessTokenTextBox.Size = new System.Drawing.Size(788, 20);
+            this.AccessTokenTextBox.TabIndex = 3;
+            // 
+            // LastNameLabel
+            // 
+            this.LastNameLabel.AutoSize = true;
+            this.LastNameLabel.Location = new System.Drawing.Point(22, 70);
+            this.LastNameLabel.Name = "LastNameLabel";
+            this.LastNameLabel.Size = new System.Drawing.Size(35, 13);
+            this.LastNameLabel.TabIndex = 4;
+            this.LastNameLabel.Text = "label1";
+            // 
+            // AvatarPictureBox
+            // 
+            this.AvatarPictureBox.Location = new System.Drawing.Point(247, 12);
+            this.AvatarPictureBox.Name = "AvatarPictureBox";
+            this.AvatarPictureBox.Size = new System.Drawing.Size(137, 140);
+            this.AvatarPictureBox.TabIndex = 5;
+            this.AvatarPictureBox.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 392);
+            this.Controls.Add(this.AvatarPictureBox);
+            this.Controls.Add(this.LastNameLabel);
+            this.Controls.Add(this.AccessTokenTextBox);
+            this.Controls.Add(this.StartWebBrowser);
+            this.Controls.Add(this.UserInfoButton);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.AvatarPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button UserInfoButton;
+        private System.Windows.Forms.WebBrowser StartWebBrowser;
+        private System.Windows.Forms.TextBox AccessTokenTextBox;
+        private System.Windows.Forms.Label LastNameLabel;
+        private System.Windows.Forms.PictureBox AvatarPictureBox;
+    }
+}
+
---
+++ testVk/testVk/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace testVk
+{
+    public partial class MainForm : Form
+    {
+        string access_token;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebClient client = new WebClient();
+            string answer = Encoding.UTF8.GetString(client.DownloadData(
+                "https://api.vk.com/method/account.getProfileInfo?"
+                +access_token
+                +"&v=5.131"
+                ));
+
+            getProfileInfo userInfo = 
+                JsonConvert.DeserializeObject<getProfileInfo>(answer);
+
+            
+            answer = Encoding.UTF8.GetString(client.DownloadData(
+               "https://api.vk.com/method/users.get?user_ids=" 
+               +userInfo.response.id  
+               +"&fields=photo_50&"
+               +access_token
+               +"&v=5.131"
+               ));
+
+            getAvatar avatar = JsonConvert.DeserializeObject<getAvatar>(answer);
+
+            LastNameLabel.Text = userInfo.response.last_name;
+            AvatarPictureBox.Load(avatar.response[0].photo_50);
+         
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            string str;
+            str = StartWebBrowser.Url.ToString();
+            if (str.Contains("access_token"))
+            {
+                //удаление всего после &
+                int AmpersandPos = str.IndexOf("&");
+
+                access_token = str.Remove(AmpersandPos);
+                //удаление всего перед #
+                int SharpPos = access_token.IndexOf("#");
+
+                access_token = access_token.Remove(0, SharpPos + 1);
+
+                AccessTokenTextBox.Text = access_token;
+            }
+        }
+
+        private void button1_Paint(object sender, PaintEventArgs e)
+        {
+            System.Drawing.Drawing2D.GraphicsPath buttonPath =
+                new System.Drawing.Drawing2D.GraphicsPath();
+
+            // Set a new rectangle to the same size as the button's 
+            // ClientRectangle property.
+            System.Drawing.Rectangle newRectangle = UserInfoButton.ClientRectangle;
+
+            // Decrease the size of the rectangle.
+            newRectangle.Inflate(-10, -1);
+
+            // Draw the button's border.
+            e.Graphics.DrawEllipse(System.Drawing.Pens.Black, newRectangle);
+
+            // Increase the size of the rectangle to include the border.
+            newRectangle.Inflate(1, 1);
+
+            // Create a circle within the new rectangle.
+            buttonPath.AddEllipse(newRectangle);
+
+            // Set the button's Region property to the newly created 
+            // circle region.
+            UserInfoButton.Region = new System.Drawing.Region(buttonPath);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+        
+        }
+    }
+}
