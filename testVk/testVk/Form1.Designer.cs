9b2a26e370d233500f644d91c8dfef9498eb9063
lissa5637 <103885918+lissa5637@users.noreply.github.com>
2022-04-17 16:25:03 +0300

    добавила кнопку, меняющую текст на ней

--- testVk/testVk/Form1.Designer.cs
+++ testVk/testVk/Form1.Designer.cs
@@ -34,14 +34,16 @@ namespace testVk
             this.AccessTokenTextBox = new System.Windows.Forms.TextBox();
             this.LastNameLabel = new System.Windows.Forms.Label();
             this.AvatarPictureBox = new System.Windows.Forms.PictureBox();
+            this.testButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.AvatarPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // UserInfoButton
             // 
-            this.UserInfoButton.Location = new System.Drawing.Point(25, 31);
+            this.UserInfoButton.Location = new System.Drawing.Point(33, 38);
+            this.UserInfoButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.UserInfoButton.Name = "UserInfoButton";
-            this.UserInfoButton.Size = new System.Drawing.Size(75, 23);
+            this.UserInfoButton.Size = new System.Drawing.Size(100, 28);
             this.UserInfoButton.TabIndex = 0;
             this.UserInfoButton.Text = "button1";
             this.UserInfoButton.UseVisualStyleBackColor = true;
@@ -50,10 +52,11 @@ namespace testVk
             // 
             // StartWebBrowser
             // 
-            this.StartWebBrowser.Location = new System.Drawing.Point(468, 31);
-            this.StartWebBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.StartWebBrowser.Location = new System.Drawing.Point(624, 38);
+            this.StartWebBrowser.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.StartWebBrowser.MinimumSize = new System.Drawing.Size(27, 25);
             this.StartWebBrowser.Name = "StartWebBrowser";
-            this.StartWebBrowser.Size = new System.Drawing.Size(345, 294);
+            this.StartWebBrowser.Size = new System.Drawing.Size(460, 362);
             this.StartWebBrowser.TabIndex = 2;
             this.StartWebBrowser.Url = new System.Uri("https://oauth.vk.com/authorize?client_id=8104769&display=page&redirect_uri=https:" +
         "//oauth.vk.com/blank.html&scope=friends&response_type=token&v=5.131&state=123456" +
@@ -62,38 +65,53 @@ namespace testVk
             // 
             // AccessTokenTextBox
             // 
-            this.AccessTokenTextBox.Location = new System.Drawing.Point(12, 357);
+            this.AccessTokenTextBox.Location = new System.Drawing.Point(16, 439);
+            this.AccessTokenTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.AccessTokenTextBox.Name = "AccessTokenTextBox";
-            this.AccessTokenTextBox.Size = new System.Drawing.Size(788, 20);
+            this.AccessTokenTextBox.Size = new System.Drawing.Size(1049, 22);
             this.AccessTokenTextBox.TabIndex = 3;
             // 
             // LastNameLabel
             // 
             this.LastNameLabel.AutoSize = true;
-            this.LastNameLabel.Location = new System.Drawing.Point(22, 70);
+            this.LastNameLabel.Location = new System.Drawing.Point(29, 86);
+            this.LastNameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LastNameLabel.Name = "LastNameLabel";
-            this.LastNameLabel.Size = new System.Drawing.Size(35, 13);
+            this.LastNameLabel.Size = new System.Drawing.Size(46, 17);
             this.LastNameLabel.TabIndex = 4;
             this.LastNameLabel.Text = "label1";
             // 
             // AvatarPictureBox
             // 
-            this.AvatarPictureBox.Location = new System.Drawing.Point(247, 12);
+            this.AvatarPictureBox.Location = new System.Drawing.Point(329, 15);
+            this.AvatarPictureBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.AvatarPictureBox.Name = "AvatarPictureBox";
-            this.AvatarPictureBox.Size = new System.Drawing.Size(137, 140);
+            this.AvatarPictureBox.Size = new System.Drawing.Size(183, 172);
             this.AvatarPictureBox.TabIndex = 5;
             this.AvatarPictureBox.TabStop = false;
             // 
+            // testButton
+            // 
+            this.testButton.Location = new System.Drawing.Point(380, 266);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(75, 23);
+            this.testButton.TabIndex = 6;
+            this.testButton.Text = "button1";
+            this.testButton.UseVisualStyleBackColor = true;
+            this.testButton.Click += new System.EventHandler(this.button1_Click_1);
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(840, 392);
+            this.ClientSize = new System.Drawing.Size(1120, 482);
+            this.Controls.Add(this.testButton);
             this.Controls.Add(this.AvatarPictureBox);
             this.Controls.Add(this.LastNameLabel);
             this.Controls.Add(this.AccessTokenTextBox);
             this.Controls.Add(this.StartWebBrowser);
             this.Controls.Add(this.UserInfoButton);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MainForm";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.AvatarPictureBox)).EndInit();
@@ -109,6 +127,7 @@ namespace testVk
         private System.Windows.Forms.TextBox AccessTokenTextBox;
         private System.Windows.Forms.Label LastNameLabel;
         private System.Windows.Forms.PictureBox AvatarPictureBox;
+        private System.Windows.Forms.Button testButton;
     }
 }
 
